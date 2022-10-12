c650c78212ea3dc6ad368011fd92a68a689b87fb
Winnie <Winnie@strathmore.com>
2022-10-12 19:43:46 +0300

    12Oct

---
+++ script.js
@@ -0,0 +1,7 @@
+*{
+    margin: 0;
+    /*box- sizing informs browser how to calculate page width*/
+    /*border-box: width and height properties*/
+    box-sizing: border-box;
+    font-family: 'Roboto', sans-serif;
+}
